1444b4d89d51ab5746da6681162dda7c20330696
MudassirK03 <160174598+MudassirK03@users.noreply.github.com>
2024-04-16 11:45:40 -0400

    homepage bottons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.medievalmath {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.desktop;
 
 
     opens org.example.medievalmath to javafx.fxml;
