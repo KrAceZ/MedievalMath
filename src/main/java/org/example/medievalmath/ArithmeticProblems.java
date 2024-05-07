9a0c5d9256261a24ac05e12b106525131b238c03
KrAceZ <zavwashington@gmail.com>
2024-05-07 19:20:33 -0400

    Update ArithmeticProblems.java

--- src/main/java/org/example/medievalmath/ArithmeticProblems.java
+++ src/main/java/org/example/medievalmath/ArithmeticProblems.java
@@ -35,11 +35,7 @@ public class ArithmeticProblems extends MathProblems<Integer> {
                 break;
         }
         operator = operators[opIndex];
-        // Check operator after it's initialized
-//        if (level == 1 && !operator.equals("+") && !operator.equals("-")) {
-//            throw new IllegalArgumentException("For level 1, operator must be '+' or '-'");
-//        }
-//        operator = operators[opIndex];
+
         switch (operator) {
             case "+":
                 answer = a + b;
@@ -52,6 +48,10 @@ public class ArithmeticProblems extends MathProblems<Integer> {
                 break;
             case "/":
                 if (b != 0) {
+                    while (a % b != 0) {
+                        a = rand.nextInt(145); // Generate a number between 0 and 144
+                        b = rand.nextInt(a + 1); // Generate a number between 0 and a
+                    }
                     answer = a / b;
                 } else {
                     throw new ArithmeticException("Cannot divide by zero.");
@@ -68,69 +68,15 @@ public class ArithmeticProblems extends MathProblems<Integer> {
     public static ArithmeticProblems generateArithmeticProblems(int level){
         ArithmeticProblems problem = new ArithmeticProblems(level);
         return problem;
-//        List<ArithmeticProblems> problems = new ArrayList<>();
-//        for(int i = 0; i< numOfProbs; i++){
-//            try {
-//                problems.add(new ArithmeticProblems(level));
-//            } catch (ArithmeticException e) {
-//                System.out.println(e.getMessage());
-//            }
-//        }
-//        return problems;
-
-
-//        Random rand = new Random();
-//        String[] operators = {"+", "-", "*", "/"};
-//        int opIndex = 0;
-//        switch (level) {
-//            case 1:
-//                a = rand.nextInt(11); // Generate a number between 0 and 10
-//                b = rand.nextInt(a + 1); // Generate a number between 0 and a
-//                opIndex = rand.nextInt(2);
-//                break;
-//            case 2:
-//                a = rand.nextInt(101); // Generate a number between 0 and 100
-//                b = rand.nextInt(101); // Generate a number between 0 and 100
-//                opIndex = rand.nextInt(4);
-//                break;
-//            case 3:
-//                a = rand.nextInt(145); // Generate a number between 0 and 144
-//                b = rand.nextInt(145); // Generate a number between 0 and 144
-//                opIndex = rand.nextInt(4)+2;
-//                break;
-//        }
-//        operator = operators[opIndex];
-//        // Check operator after it's initialized
-//        if (level == 1 && !operator.equals("+") && !operator.equals("-")) {
-//            throw new IllegalArgumentException("For level 1, operator must be '+' or '-'");
-//        }
-//        operator = operators[opIndex];
-//        switch (operator) {
-//            case "+":
-//                answer = a + b;
-//                break;
-//            case "-":
-//                answer = a - b;
-//                break;
-//            case "*":
-//                answer = a * b;
-//                break;
-//            case "/":
-//                if (b != 0) {
-//                    answer = a / b;
-//                } else {
-//                    throw new ArithmeticException("Cannot divide by zero.");
-//                }
-//                break;
-//            default:
-//                throw new IllegalArgumentException("Invalid operator. Must be '+', '-', '*', '/'");
-//        }
-//        generateOptions();
     }
 
     @Override
     public String getProblem() {
-        return a + " " + operator + " " + b + " = ";
+        String displayOperator = operator;
+        if (operator.equals("/")) {
+            displayOperator = "÷";
+        }
+        return a + " " + displayOperator + " " + b + " = ";
     }
 
     protected void generateOptions() {
